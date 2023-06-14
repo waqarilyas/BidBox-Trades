4f54186d25ab060a862bae7b05e87bf8eee3ce24
shaheer <shaheer.ahmad@kryptomind.com>
2023-06-14 19:19:21 +0500

    Chore: quantity precision for binance

--- controllers/home.go
+++ controllers/home.go
@@ -22,6 +22,7 @@ import (
 	"github.com/kryptomind/BidBox-Trades/response"
 	"github.com/kryptomind/BidBox-Trades/utils"
 	log "github.com/sirupsen/logrus"
+	"github.com/kryptomind/BidBox-Trades/exchange/binance"
 )
 
 var mutex sync.Mutex
@@ -270,7 +271,7 @@ func fetchAndUpdateBitgetPosition(order models.OrderRequest, v models.Key, db *g
 }
 
 func binanceTrade(s *Server, v models.Key, i int, trade_req *TradeRequest, first_order float64, res map[string]string, w http.ResponseWriter, keys []models.Key) {
-
+	
 	futures.UseTestnet = true
 	api_key, secret_key, _, err := utils.DecryptKeys(v.ApiKey, v.SecretKey, v.Passphrase, "binance")
 	if err != nil {
@@ -333,9 +334,16 @@ func binanceTrade(s *Server, v models.Key, i int, trade_req *TradeRequest, first
 		positionSide = "BOTH"
 	}
 
-	strValue := strconv.FormatFloat(x, 'f', 3, 64)
-
-	ordersResponse, takeProfit, stopLoss, error := handleBinanceMultiOrder(api_key, secret_key, strValue, symbol, p, positionSide, side)
+	// strValue := strconv.FormatFloat(x, 'f', 3, 64)
+	stepSize, err := binance.GetBinanceTradeLimit(symbol2)
+	if err != nil{
+		println("error in calculating limit", stepSize)
+		return 
+	}
+	precision := int(math.Round(-math.Log10(stepSize)))
+	quantity := math.Round(x*math.Pow(10, float64(precision))) / math.Pow(10, float64(precision))
+	quantity_str := fmt.Sprintf("%f", quantity)
+	ordersResponse, takeProfit, stopLoss, error := handleBinanceMultiOrder(api_key, secret_key, quantity_str, symbol, p, positionSide, side)
 	if error != nil {
 		log.Error("error in order: " + err.Error() + " for " + v.UserEmail)
 		fmt.Println("error in create order", err)
